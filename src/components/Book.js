034c8eadf24456b8340b50299967904fa80a7735
trance <michaelmunavu83@gmail.com>
2022-07-12 17:30:58 +0300

    Add button to categories.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Form from './Form';
 
 function Book({ books }) {
   return (
@@ -13,6 +14,7 @@ function Book({ books }) {
           {book.Author}
         </div>
       ))}
+      <Form />
 
     </div>
   );
