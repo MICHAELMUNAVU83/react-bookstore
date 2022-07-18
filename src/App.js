8c162607ff203a108b597236d2fdd307207cadd5
trance <michaelmunavu83@gmail.com>
2022-07-18 19:09:17 +0300

    add add book functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import './App.css';
 import Categories from './components/Categories';
 import Navbar from './components/Navbar';
 import Book from './components/Book';
 
 function App() {
-  const [books, setBooks] = useState([
-    {
-      Title: 'Harry Potter',
-      Author: 'Jk Rowling',
-      id: Date.now(),
-
-    },
-
-  ]);
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Book books={books} setBooks={setBooks} />} />
+          <Route path="/" element={<Book />} />
           <Route path="/categories" element={<Categories />} />
         </Routes>
       </Router>
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,23 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Form from './Form';
 
-function Book({ books }) {
+function Book() {
+  const books = useSelector((state) => state.books);
+  const rendereach = books.map((book) => (
+    <div key={book.id}>
+      <span>{book.title}</span>
+      {' '}
+      <span>by</span>
+      {' '}
+      <span>{book.author}</span>
+      <button type="button">DELETE</button>
+    </div>
+  ));
   return (
     <div>
-      {books.map((book) => (
-        <div key={book.id}>
-          {book.Title}
-          {' '}
-          by
-          {' '}
-          {book.Author}
-        </div>
-      ))}
+      {rendereach}
+
       <Form />
 
     </div>
