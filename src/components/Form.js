eb696ab33df425296e19757d9082d90629bf58b5
trance <michaelmunavu83@gmail.com>
2022-07-18 18:49:26 +0300

    Added form dispatch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function Form() {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const addaBook = () => {
+    dispatch(addBook({
+      title,
+      author,
+      id: Date.now(),
+
+    }));
+
+    setTitle('');
+    setAuthor('');
+  };
   return (
     <div>
       <div>
         <h3>ADD A NEW BOOK</h3>
       </div>
       <div>
-        <input placeholder="Book title" />
-        <input placeholder="Author" />
-        <button type="button">add book</button>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Book title" />
+        <input value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
+        <button onClick={addaBook} type="button">add book</button>
 
       </div>
 
